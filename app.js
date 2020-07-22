ab6eaf5f5109f64ee751d76b07d67516cc2e318d
Damilare Richard Olagbende <damilareshinaayo@gmail.com>
2020-07-23 00:43:44 +0100

    IGHUB

---
+++ app.js
@@ -0,0 +1,8 @@
+const hamburger = document.getElementById('hamburger');
+const navUL = document.getElementById('navLinks');
+
+
+hamburger.addEventListener('click', () => {
+	navUL.classList.toggle('change');
+	
+});
